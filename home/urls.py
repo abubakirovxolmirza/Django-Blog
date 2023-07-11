d0ba531438b3743cc50643657e27c67253e95ba2
abubakirovxolmirza <137431926+abubakirovxolmirza@users.noreply.github.com>
2023-07-11 19:15:15 +0500

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
 
 #    user authentication
     path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="logout"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
 ]
